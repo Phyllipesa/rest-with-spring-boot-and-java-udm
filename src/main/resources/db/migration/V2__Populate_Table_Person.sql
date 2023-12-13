83d4037a896d799bd216e3378d93e29c40458f20
Phyllipe <phyllipe.sjc@hotmail.com>
2023-12-13 14:56:04 -0300

    Adding migrations

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,10 @@
+INSERT INTO `person` (`id`, `first_name`, `last_name`, `address`, `gender`) VALUES
+    (1, 'Ayrton', 'Senna', 'São Paulo', 'Male'),
+    (2, 'Leonardo', 'da Vinci', 'Anchiano - Italy', 'Male'),
+    (3, 'Indira', 'Gandhi', 'Porbandar - India', 'Female'),
+    (4, 'Mahatma', 'Gandhi', 'Porbandar - India', 'Male'),
+    (5, 'Muhammad', 'Ali', 'Kentucky - US', 'Male'),
+    (6, 'Nelson', 'Mandela', 'Mvezo - South Africa', 'Male'),
+    (7, 'Nikola', 'Tesla', 'Smiljan - Croacia', 'Male'),
+    (8, 'Yuri', 'Gagari', 'Klushino - URSS', 'Male'),
+    (9, 'Marie', 'Curie', 'Warsaw - Poland', 'Female');
