f85970c70fee2f7571908e2adf50c07b6b24e4cc
Phyllipe <phyllipe.sjc@hotmail.com>
2023-12-20 17:45:53 -0300

    Refactor ModelMapperConfig, add book config

--- src/main/java/com/phyllipesa/erudio/util/ModelMapperConfig.java
+++ src/main/java/com/phyllipesa/erudio/util/ModelMapperConfig.java
@@ -1,6 +1,8 @@
 package com.phyllipesa.erudio.util;
 
+import com.phyllipesa.erudio.data.vo.v1.BookVO;
 import com.phyllipesa.erudio.data.vo.v1.PersonVO;
+import com.phyllipesa.erudio.models.Book;
 import com.phyllipesa.erudio.models.Person;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +17,10 @@ public class ModelMapperConfig {
     modelMapper.typeMap(Person.class, PersonVO.class)
         .addMappings(mapper -> {
           mapper.map(Person::getId, PersonVO::setKey);
+        });
+    modelMapper.typeMap(Book.class, BookVO.class)
+        .addMappings(mapper -> {
+          mapper.map(Book::getId, BookVO::setKey);
         });
     return modelMapper;
   }
