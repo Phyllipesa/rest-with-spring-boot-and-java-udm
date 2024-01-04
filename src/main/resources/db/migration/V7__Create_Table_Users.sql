3627ffc749cf37f3800fa05ae28294a9d0b4edf3
Phyllipe <phyllipe.sjc@hotmail.com>
2024-01-04 16:11:06 -0300

    Created migration Users table

---
+++ src/main/resources/db/migration/V7__Create_Table_Users.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` INT(20) AUTO_INCREMENT,
+    `user_name` VARCHAR(255) DEFAULT NULL,
+    `full_name` VARCHAR(255) DEFAULT NULL,
+    `password` VARCHAR(255) DEFAULT NULL,
+    `account_non_expired` BIT(1) DEFAULT NULL,
+    `account_non_locked` BIT(1) DEFAULT NULL,
+    `credentials_non_expired` BIT(1) DEFAULT NULL,
+    `enable` BIT(1) DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `uk_user_name` (`user_name`)
+) ENGINE=InnoDB;
