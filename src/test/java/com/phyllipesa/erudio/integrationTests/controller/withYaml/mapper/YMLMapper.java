cdfc7a5cc18f9bde41f7366712986b4736ccd335
Phyllipe <phyllipe.sjc@hotmail.com>
2024-01-09 18:05:37 -0300

    Including 'new YAMLFactory() Parameter in ObjectMapper Method

--- src/test/java/com/phyllipesa/erudio/integrationTests/controller/withYaml/mapper/YMLMapper.java
+++ src/test/java/com/phyllipesa/erudio/integrationTests/controller/withYaml/mapper/YMLMapper.java
@@ -2,19 +2,18 @@ package com.phyllipesa.erudio.integrationTests.controller.withYaml.mapper;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import io.restassured.mapper.ObjectMapper;
 import io.restassured.mapper.ObjectMapperDeserializationContext;
 import io.restassured.mapper.ObjectMapperSerializationContext;
 
 public class YMLMapper implements ObjectMapper {
-
   private final com.fasterxml.jackson.databind.ObjectMapper objectMapper;
   protected TypeFactory typeFactory;
 
-  public YMLMapper(TypeFactory typeFactory) {
-    this.objectMapper = new com.fasterxml.jackson.databind.ObjectMapper();
+  public YMLMapper() {
+    this.objectMapper = new com.fasterxml.jackson.databind.ObjectMapper(new YAMLFactory());
     objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
     typeFactory = TypeFactory.defaultInstance();
   }
