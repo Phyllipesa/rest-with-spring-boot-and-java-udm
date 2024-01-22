c1f89da026fd7de3d1008baccc0b834e7a092a30
Phyllipe <phyllipe.sjc@hotmail.com>
2024-01-22 15:34:02 -0300

    Implemented FileStorageService

--- src/main/java/com/phyllipesa/erudio/exceptions/FileStorageException.java
+++ src/main/java/com/phyllipesa/erudio/exceptions/FileStorageException.java
@@ -6,15 +6,15 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import java.io.Serial;
 
 @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-public class FIleStorageException extends RuntimeException {
+public class FileStorageException extends RuntimeException {
   @Serial
   private static final long serialVersionUID = 1L;
 
-  public FIleStorageException(String e) {
+  public FileStorageException(String e) {
     super(e);
   }
 
-  public FIleStorageException(String e, Throwable cause) {
+  public FileStorageException(String e, Throwable cause) {
     super(e, cause);
   }
 }
