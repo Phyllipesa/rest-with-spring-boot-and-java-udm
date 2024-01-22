a71e1c545f0b2a841592f4d2de201d456c39f9f2
Phyllipe <phyllipe.sjc@hotmail.com>
2024-01-22 13:50:09 -0300

    Implemented UploadFileResponseVO

---
+++ src/main/java/com/phyllipesa/erudio/data/vo/v1/UploadFileResponseVO.java
@@ -0,0 +1,57 @@
+package com.phyllipesa.erudio.data.vo.v1;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class UploadFileResponseVO implements Serializable {
+
+  @Serial
+  private static final long serialVersionUID = 1L;
+
+  private String fileName;
+  private String fileDownloadUri;
+  private String fileType;
+  private String size;
+
+  public UploadFileResponseVO() {
+  }
+
+  public UploadFileResponseVO(String fileName, String fileDownloadUri, String fileType, String size) {
+    this.fileName = fileName;
+    this.fileDownloadUri = fileDownloadUri;
+    this.fileType = fileType;
+    this.size = size;
+  }
+
+  public String getFileName() {
+    return fileName;
+  }
+
+  public void setFileName(String fileName) {
+    this.fileName = fileName;
+  }
+
+  public String getFileDownloadUri() {
+    return fileDownloadUri;
+  }
+
+  public void setFileDownloadUri(String fileDownloadUri) {
+    this.fileDownloadUri = fileDownloadUri;
+  }
+
+  public String getFileType() {
+    return fileType;
+  }
+
+  public void setFileType(String fileType) {
+    this.fileType = fileType;
+  }
+
+  public String getSize() {
+    return size;
+  }
+
+  public void setSize(String size) {
+    this.size = size;
+  }
+}
