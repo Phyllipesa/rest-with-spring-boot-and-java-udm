1ae18e6bbe9137b1ec53a4913517e5d420892bd8
Phyllipe <phyllipe.sjc@hotmail.com>
2024-01-22 15:55:02 -0300

    Implemented FileController for upload and download files

---
+++ src/main/java/com/phyllipesa/erudio/controllers/FileController.java
@@ -0,0 +1,42 @@
+package com.phyllipesa.erudio.controllers;
+
+import com.phyllipesa.erudio.data.vo.v1.UploadFileResponseVO;
+import com.phyllipesa.erudio.services.FileStorageService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.logging.Logger;
+
+@Tag(name = "File Endpoint")
+@RestController
+@RequestMapping("/api/file/v1")
+public class FileController {
+
+  private Logger logger = Logger.getLogger(FileController.class.getName());
+
+  @Autowired
+  private FileStorageService fileStorageService;
+
+  @PostMapping("/uploadFile")
+  public UploadFileResponseVO uploadFile(@RequestParam("file")MultipartFile file) {
+    logger.info("Storing file to disk");
+    var filename = fileStorageService.storeFile(file);
+    String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+        .path("/api/file/v1/downloadFile/")
+        .path(filename)
+        .toUriString();
+
+    return new UploadFileResponseVO(
+        filename,
+        fileDownloadUri,
+        file.getContentType(),
+        file.getSize());
+  }
+
+}
